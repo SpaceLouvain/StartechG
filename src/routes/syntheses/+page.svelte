a27a030182d5bdfdec17a6b20dcbdd3f99ec7a3b
Loremacle1 <louis.remacle@student.uclouvain.be>
2024-04-23 12:14:57 +0200

    jj

---
+++ src/routes/syntheses/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  let message = "Nous n'avons pas encore de synthèse à proposer";
+</script>
+
+<main>
+  <h1>{message}</h1>
+</main>
+
--- src/routes/test/chimie2023/+page.svelte
+++ src/routes/test/chimie2023/+page.svelte
@@ -140,9 +140,9 @@
 
 <h1>CHIMIE 2023</h1>
 
-<a  class="image-link" on:click={handleImageClick}>
+<div  class="image-link" on:click={handleImageClick}>
     <img src="/./ampoule.png" alt="gay">
-</a>
+</div>
 
 <main>
     <div class="center-content">
